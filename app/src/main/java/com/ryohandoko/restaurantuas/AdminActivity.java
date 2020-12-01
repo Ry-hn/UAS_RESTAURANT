ba1090323ce0319c740d40a5111a84ee9f873541
Ry-hn <aditiaryo@gmail.com>
2020-12-01 11:07:13 +0700

    admin page

--- app/src/main/java/com/ryohandoko/restaurantuas/AdminActivity.java
+++ app/src/main/java/com/ryohandoko/restaurantuas/AdminActivity.java
@@ -1,19 +1,46 @@
 package com.ryohandoko.restaurantuas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.lifecycle.Observer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.ryohandoko.restaurantuas.repository.ProductRepository;
+import com.ryohandoko.restaurantuas.view.admin.CreateProductActivity;
+import com.ryohandoko.restaurantuas.view.admin.CreateUserActivity;
 
-public class AdminActivity extends AppCompatActivity {
+public class AdminActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CardView createUser, createProduct, showUser, showProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        createUser = findViewById(R.id.cvCreateuser);
+        createProduct = findViewById(R.id.cvCreateProduct);
+        showUser = findViewById(R.id.cvShowListUser);
+        showProduct = findViewById(R.id.cvShowListProduct);
+
+        createUser.setOnClickListener(this);
+        createProduct.setOnClickListener(this);
+    }
+
+    private void loadActivity(Class<?> clazz) {
+        Intent i = new Intent(this, clazz);
+        startActivity(i);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.cvCreateuser: loadActivity(CreateUserActivity.class); break;
+            case R.id.cvCreateProduct: loadActivity(CreateProductActivity.class); break;
+        }
     }
 }
---
+++ app/src/main/java/com/ryohandoko/restaurantuas/view/admin/CreateProductActivity.java
@@ -0,0 +1,16 @@
+package com.ryohandoko.restaurantuas.view.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ryohandoko.restaurantuas.R;
+
+public class CreateProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_product);
+    }
+}
